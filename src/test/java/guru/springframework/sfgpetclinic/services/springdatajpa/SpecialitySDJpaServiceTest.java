c88872416501d0c5f61108b04a00d864704da798
Saahas Kulkarni <saa.kulkarni@gmail.com>
2019-05-25 17:12:07 +0530

    Using Mockito.verify() to verify interactions with mock

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -1,10 +1,13 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
-import org.junit.jupiter.api.Disabled;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.times;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import guru.springframework.sfgpetclinic.model.Speciality;
@@ -19,16 +22,24 @@ public class SpecialitySDJpaServiceTest {
 	@Mock
 	private SpecialtyRepository specialtyRepo;
 	
-	@Disabled
 	@Test
 	public void testDelete() {
-		service.delete(new Speciality());
+		Speciality speciality = new Speciality();
+		service.delete(speciality);
+		
+		//Mockito.verify(specialtyRepo).delete(speciality);
+		Mockito.verify(specialtyRepo, atLeastOnce()).delete(speciality);
 		
 	}
 
 	@Test
 	public void testDeleteById() {
 		service.deleteById(1L);
+		service.deleteById(1L);
+		
+		Mockito.verify(specialtyRepo, times(2)).deleteById(1L);
+		Mockito.verify(specialtyRepo, Mockito.atMost(3)).deleteById(1L);
+		Mockito.verify(specialtyRepo,  times(0)).delete(new Speciality());
 	}
 
 }
